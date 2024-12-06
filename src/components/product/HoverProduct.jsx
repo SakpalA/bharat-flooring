417eef2742e6f88452861c8c0fbbc260b4619c17
SakpalA <akankshasakpal29@gmail.com>
2024-12-06 14:15:39 +0530

    update hover images

--- src/components/product/HoverProduct.jsx
+++ src/components/product/HoverProduct.jsx
@@ -11,10 +11,10 @@ const HoverProduct = () => {
             <span className='hover-span'>30 colors</span><span className='sproduct-span'>30 colors</span>
             <h4 className='hover-head'>Designer tiles</h4>
             <div className='hover-img-div'>
-                <img src={hover1} alt="Hover image" className='hover-img' />
                 <img src={hover2} alt="Hover image" className='hover-img' />
                 <img src={hover3} alt="Hover image" className='hover-img' />
                 <img src={hover4} alt="Hover image" className='hover-img' />
+                <img src={hover1} alt="Hover image" className='hover-img' />
             </div>
             <p className='hover-text'>From exquisite handcrafted cement tiles that bring timeless beauty to your space, to elegant terrazzo floors that exude grandeur, our collection of decorative concrete, outdoor floors, micro cement overlays, wallpapers, bespoke products, and more, offers limitless design possibilities. Let your imagination soar as you discover the perfect flooring solution that transforms ordinary spaces into extraordinary works of art.
             </p>
