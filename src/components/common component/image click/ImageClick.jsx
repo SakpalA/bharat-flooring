6ed97242e3d8e7a1a2a7d8527a7a754134ec9a61
SakpalA <akankshasakpal29@gmail.com>
2024-12-06 14:06:59 +0530

    update style

--- src/components/common component/image click/ImageClick.jsx
+++ src/components/common component/image click/ImageClick.jsx
@@ -10,24 +10,24 @@ const ImageClick = ({image1, image2, image3}) => {
                 <img src={currentImage} alt="" className='current-img' />
             </div>
             <div className='all-img'>
-                <div className='img-div'>
+                <div className={`img-div ${currentImage === image1 ? "active" : ""}`}>
                     <img src={image1}
                         alt="Bathroom"
-                        className='img'
+                        className={`img ${currentImage === image1 ? 'zoom' : ''}`}
                         onClick={() => setCurrentImage(image1)} />
                 </div>
 
-                <div className="img-div">
+                <div className={`img-div ${currentImage === image2 ? "active" : ""}`}>
                     <img src={image2}
                         alt="Table"
-                        className='img'
+                        className={`img ${currentImage === image2 ? 'zoom' : ''}`}
                         onClick={() => setCurrentImage(image2)} />
                 </div>
 
-                <div className="img-div">
+                <div className={`img-div ${currentImage === image3 ? "active" : ""}`}>
                     <img src={image3}
                         alt="Bedroom"
-                        className='img'
+                        className={`img ${currentImage === image3 ? 'zoom' : ''}`}
                         onClick={() => setCurrentImage(image3)} />
                 </div>
 
